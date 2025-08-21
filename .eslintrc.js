f9e907d17f21c14253666e2848df8aad999620e2
ankitp5 <ankitp@eccentricengine.com>
2025-08-21 12:28:19 +0530

    package update

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  extends: [require.resolve('@ionic/eslint-config/recommended')],
+  ignorePatterns: [
+    '.eslintrc.js',
+    'android/**',
+    'ios/**',
+    'dist/**',
+    'scripts/**',
+    'bin/**',
+    'node_modules/**',
+  ],
+};
